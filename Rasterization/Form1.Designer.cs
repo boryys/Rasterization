5e93b260c989918a492045f060c51d0f633dc82e
UlaBorys <ula_borys@op.pl>
2018-04-18 10:22:55 +0200

    GUI, circle drawing, thick line drawing

---
+++ Rasterization/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Rasterization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.comboBoxT = new System.Windows.Forms.ComboBox();
+            this.comboBoxA = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tLineButton = new System.Windows.Forms.RadioButton();
+            this.antiAlButton = new System.Windows.Forms.RadioButton();
+            this.CircleButton = new System.Windows.Forms.RadioButton();
+            this.LineButton = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(962, 540);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.clearButton);
+            this.panel1.Controls.Add(this.applyButton);
+            this.panel1.Controls.Add(this.comboBoxT);
+            this.panel1.Controls.Add(this.comboBoxA);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.tLineButton);
+            this.panel1.Controls.Add(this.antiAlButton);
+            this.panel1.Controls.Add(this.CircleButton);
+            this.panel1.Controls.Add(this.LineButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(814, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(146, 536);
+            this.panel1.TabIndex = 0;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(10, 479);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(126, 40);
+            this.clearButton.TabIndex = 14;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // applyButton
+            // 
+            this.applyButton.Location = new System.Drawing.Point(10, 420);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(126, 40);
+            this.applyButton.TabIndex = 13;
+            this.applyButton.Text = "Apply";
+            this.applyButton.UseVisualStyleBackColor = true;
+            this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
+            // 
+            // comboBoxT
+            // 
+            this.comboBoxT.FormattingEnabled = true;
+            this.comboBoxT.Items.AddRange(new object[] {
+            "1",
+            "3",
+            "5",
+            "7"});
+            this.comboBoxT.Location = new System.Drawing.Point(86, 369);
+            this.comboBoxT.Name = "comboBoxT";
+            this.comboBoxT.Size = new System.Drawing.Size(46, 21);
+            this.comboBoxT.TabIndex = 12;
+            this.comboBoxT.Text = "1";
+            // 
+            // comboBoxA
+            // 
+            this.comboBoxA.FormattingEnabled = true;
+            this.comboBoxA.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.comboBoxA.Location = new System.Drawing.Point(86, 335);
+            this.comboBoxA.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.comboBoxA.Name = "comboBoxA";
+            this.comboBoxA.Size = new System.Drawing.Size(46, 21);
+            this.comboBoxA.TabIndex = 11;
+            this.comboBoxA.Text = "1";
+
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 302);
+            this.label5.Margin = new System.Windows.Forms.Padding(3, 20, 3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Choose thickness";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 372);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Thick line";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 338);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Anti-aliasing";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 206);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 20, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Choose points";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Location = new System.Drawing.Point(76, 239);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 20, 10, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "second point";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(10, 239);
+            this.button1.Margin = new System.Windows.Forms.Padding(10, 20, 3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "first point";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tLineButton
+            // 
+            this.tLineButton.AutoSize = true;
+            this.tLineButton.Location = new System.Drawing.Point(21, 166);
+            this.tLineButton.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.tLineButton.Name = "tLineButton";
+            this.tLineButton.Size = new System.Drawing.Size(111, 17);
+            this.tLineButton.TabIndex = 4;
+            this.tLineButton.TabStop = true;
+            this.tLineButton.Text = "Thick line drawing";
+            this.tLineButton.UseVisualStyleBackColor = true;
+            this.tLineButton.CheckedChanged += new System.EventHandler(this.tLineButton_CheckedChanged);
+            // 
+            // antiAlButton
+            // 
+            this.antiAlButton.AutoSize = true;
+            this.antiAlButton.Location = new System.Drawing.Point(21, 126);
+            this.antiAlButton.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.antiAlButton.Name = "antiAlButton";
+            this.antiAlButton.Size = new System.Drawing.Size(81, 17);
+            this.antiAlButton.TabIndex = 3;
+            this.antiAlButton.TabStop = true;
+            this.antiAlButton.Text = "Anit-aliasing";
+            this.antiAlButton.UseVisualStyleBackColor = true;
+            this.antiAlButton.CheckedChanged += new System.EventHandler(this.antiAlButton_CheckedChanged);
+            // 
+            // CircleButton
+            // 
+            this.CircleButton.AutoSize = true;
+            this.CircleButton.Location = new System.Drawing.Point(21, 86);
+            this.CircleButton.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.CircleButton.Name = "CircleButton";
+            this.CircleButton.Size = new System.Drawing.Size(91, 17);
+            this.CircleButton.TabIndex = 2;
+            this.CircleButton.TabStop = true;
+            this.CircleButton.Text = "Circle drawing";
+            this.CircleButton.UseVisualStyleBackColor = true;
+            this.CircleButton.CheckedChanged += new System.EventHandler(this.CircleButton_CheckedChanged);
+            // 
+            // LineButton
+            // 
+            this.LineButton.AutoSize = true;
+            this.LineButton.Location = new System.Drawing.Point(21, 46);
+            this.LineButton.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(85, 17);
+            this.LineButton.TabIndex = 1;
+            this.LineButton.TabStop = true;
+            this.LineButton.Text = "Line drawing";
+            this.LineButton.UseVisualStyleBackColor = true;
+            this.LineButton.CheckedChanged += new System.EventHandler(this.LineButton_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Options";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(810, 536);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 562);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 600);
+            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rasterization";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button applyButton;
+        private System.Windows.Forms.ComboBox comboBoxT;
+        private System.Windows.Forms.ComboBox comboBoxA;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton tLineButton;
+        private System.Windows.Forms.RadioButton antiAlButton;
+        private System.Windows.Forms.RadioButton CircleButton;
+        private System.Windows.Forms.RadioButton LineButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Rasterization/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rasterization
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Graphics grp;
+        Point p1;
+        Point p2;
+        Button whichP;
+        int pointSize = 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxA.Enabled = false;
+            comboBoxT.Enabled = false;
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            grp = Graphics.FromImage(bmp);
+        }
+
+        private void drawLine()
+        {
+            int x1 = 0, x2 = 0, y1 = 0, y2 = 0, yy = 0;
+            if (p1.X <= p2.X)
+            {
+                x1 = p1.X;
+                x2 = p2.X;
+                y1 = p1.Y;
+                y2 = p2.Y;
+            }
+            else
+            {
+                x1 = p2.X;
+                x2 = p1.X;
+                y1 = p2.Y;
+                y2 = p1.Y;
+            }
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+
+            int d = 2 * dy - dx; // initial value of d
+            int dE = 2 * dy; // increment used when moving to E
+            int dNE = 2 * (dy - dx); // increment used when movint to NE
+            int x = x1, y = y1;
+            bmp.SetPixel(x, y, Color.Black);
+            while (x < x2)
+            {
+                if (d < 0) // move to E
+                {
+                    d += dE;
+                    x++;
+                }
+                else // move to NE
+                {
+                    d += dNE;
+                    x++;
+                    y++;
+                }
+                bmp.SetPixel(x, y, Color.Black);
+            }
+        }
+
+        private void drawCircle()
+        {
+            int R = (int)Math.Sqrt((p1.X - p2.X)* (p1.X - p2.X) + (p1.Y - p2.Y)*(p1.Y - p2.Y));
+
+            int d = 1 - R;
+            int x = 0;
+            int y = R;
+
+            try { bmp.SetPixel(p1.X + R, p1.Y, Color.Black); } catch { }
+            try { bmp.SetPixel(p1.X - R, p1.Y, Color.Black); } catch { }
+            try { bmp.SetPixel(p1.X, p1.Y + R, Color.Black); } catch { }
+            try { bmp.SetPixel(p1.X, p1.Y - R, Color.Black); } catch { }
+            while (y > x)
+            {
+                if (d < 0)
+                    d += 2 * x + 3;
+                else
+                {
+                    d += 2 * x - 2 * y + 5;
+                    --y;
+                }
+                ++x;
+
+                try{ bmp.SetPixel(p1.X + x, p1.Y + y, Color.Black); } catch { }
+                try{ bmp.SetPixel(p1.X + y, p1.Y + x, Color.Black); } catch { }
+                try{ bmp.SetPixel(p1.X + y, p1.Y - x, Color.Black); } catch { }
+                try{ bmp.SetPixel(p1.X + x, p1.Y - y, Color.Black); } catch { }
+                try{ bmp.SetPixel(p1.X - x, p1.Y - y, Color.Black); } catch { }
+                try{ bmp.SetPixel(p1.X - y, p1.Y - x, Color.Black); } catch { }
+                try{ bmp.SetPixel(p1.X - y, p1.Y + x, Color.Black); } catch { }
+                try{ bmp.SetPixel(p1.X - x, p1.Y + y, Color.Black); } catch { }
+            }
+        }
+
+        private void drawThickLine(int thickness)
+        {
+            int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+            if (Math.Abs(p2.X - p1.X) >= Math.Abs(p2.Y - p1.Y))
+            {
+                if (p1.X <= p2.X)
+                {
+                    x1 = p1.X;
+                    x2 = p2.X;
+                    y1 = p1.Y;
+                    y2 = p2.Y;
+                }
+                else
+                {
+                    x1 = p2.X;
+                    x2 = p1.X;
+                    y1 = p2.Y;
+                    y2 = p1.Y;
+                }
+
+                float dy = y2 - y1;
+                float dx = x2 - x1;
+                float m = dy / dx;
+                float y = y1;
+                for (int x = x1; x <= x2; ++x)
+                {
+                    bmp.SetPixel(x, (int)y, Color.Black);
+                    for (int i = 1; i <= thickness / 2; i++)
+                    {
+                        bmp.SetPixel(x, (int)y + i, Color.Black);
+                        bmp.SetPixel(x, (int)y - i, Color.Black);
+                    }
+                    y += m;
+                }
+            }
+            else
+            {
+                if (p1.Y <= p2.Y)
+                {
+                    x1 = p1.X;
+                    x2 = p2.X;
+                    y1 = p1.Y;
+                    y2 = p2.Y;
+                }
+                else
+                {
+                    x1 = p2.X;
+                    x2 = p1.X;
+                    y1 = p2.Y;
+                    y2 = p1.Y;
+                }
+
+                float dy = y2 - y1;
+                float dx = x2 - x1;
+                float m = dx / dy;
+                float x = x1;
+                for (int y = y1; y <= y2; ++y)
+                {
+                    bmp.SetPixel((int)x, y, Color.Black);
+                    for (int i = 1; i <= thickness / 2; i++)
+                    {
+                        bmp.SetPixel((int)x + i, y, Color.Black);
+                        bmp.SetPixel((int)x - i, y, Color.Black);
+                    }
+                    x += m;
+                }
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            pictureBox1.Image = bmp;
+        }
+
+        public void SetPixel(SolidBrush brush, Graphics grp, int x, int y, Color color)
+        {
+            brush.Color = color;
+            grp.FillRectangle(brush, x, y, 2, 2);
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (whichP == button1)
+                {
+                    grp.DrawEllipse(Pens.White, p1.X - pointSize / 2, p1.Y - pointSize / 2, pointSize, pointSize);
+                    grp.FillEllipse(new SolidBrush(Color.White), p1.X - pointSize / 2, p1.Y - pointSize / 2, pointSize, pointSize);
+                    p1 = new Point(e.X, e.Y);
+                    grp.DrawEllipse(Pens.Red, p1.X - pointSize / 2, p1.Y - pointSize / 2, pointSize, pointSize);
+                    grp.FillEllipse(new SolidBrush(Color.Red), p1.X - pointSize / 2, p1.Y - pointSize / 2, pointSize, pointSize);
+                }
+                else
+                {
+                    if (whichP == button2)
+                    {
+                        grp.DrawEllipse(Pens.White, p2.X - pointSize / 2, p2.Y - pointSize / 2, pointSize, pointSize);
+                        grp.FillEllipse(new SolidBrush(Color.White), p2.X - pointSize / 2, p2.Y - pointSize / 2, pointSize, pointSize);
+                        p2 = new Point(e.X, e.Y);
+                        grp.DrawEllipse(Pens.Red, p2.X - pointSize / 2, p2.Y - pointSize / 2, pointSize, pointSize);
+                        grp.FillEllipse(new SolidBrush(Color.Red), p2.X - pointSize / 2, p2.Y - pointSize / 2, pointSize, pointSize);
+                    }
+                }
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            grp = Graphics.FromImage(bmp);
+            button1.BackColor = Color.Silver;
+            button2.BackColor = Color.Silver;
+
+            p1 = new Point();
+            p2 = new Point();
+            whichP = null;
+        }
+
+        private void antiAlButton_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBoxA.Enabled = true;
+            comboBoxT.Enabled = false;
+        }
+
+        private void tLineButton_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBoxA.Enabled = false;
+            comboBoxT.Enabled = true;
+        }
+
+        private void CircleButton_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBoxA.Enabled = false;
+            comboBoxT.Enabled = false;
+        }
+
+        private void LineButton_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBoxA.Enabled = false;
+            comboBoxT.Enabled = false;
+        }
+
+        private void applyButton_Click(object sender, EventArgs e)
+        {
+            if(LineButton.Checked)
+            {
+                drawLine();
+            }
+            if (CircleButton.Checked)
+            {
+                drawCircle();
+            }
+            if (antiAlButton.Checked)
+            {
+                
+            }
+            if (tLineButton.Checked)
+            {
+                drawThickLine(Int32.Parse(comboBoxT.Text));
+            }
+
+            p1 = new Point();
+            p2 = new Point();
+            whichP = null;
+
+            button1.BackColor = Color.Silver;
+            button2.BackColor = Color.Silver;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            whichP = button1;
+            button1.BackColor = Color.HotPink;
+
+            if (p2.IsEmpty) button2.BackColor = Color.Silver;
+            else button2.BackColor = Color.PaleGreen;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            whichP = button2;
+            button2.BackColor = Color.HotPink;
+
+            if (p1.IsEmpty) button1.BackColor = Color.Silver;
+            else button1.BackColor = Color.PaleGreen;
+        }
+        
+    }
+}
+
